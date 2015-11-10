10bb273381467c01b7a144a2dccadfd67f904a5a
Jon Nolen <jon.nolen@gmail.com>
2015-11-10 13:11:18 -0500

    adding env to fork.

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 var cluster = require('cluster'),
     _ = require('underscore');
 
+var worker_type = "worker_type";
+
 if (cluster.isMaster) {
   console.log('MASTER OF THE UNIVERSE!');
+  var env = {};
+  env[worker_type] =  "WORKER DATA YO";
   _(3).times(function(){
-    cluster.fork();
+    cluster.fork(env);
   });
 }
 else {
-  console.log("worker started, id: %j, pid: %j", cluster.worker.id, process.pid);
+  console.log("worker started, id: %j, pid: %j worker_type: %j", cluster.worker.id, process.pid, process.env[worker_type]);
 }
